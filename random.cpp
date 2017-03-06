c33bcbf06f281c701179d86f08cda840a57ec56a
Ahtesham Ul Haq(@Ahtesham519) <hptouchpad519@gmail.com>
2017-03-06 18:52:46 +0500

    Create random.cpp
    enjoy the code ,and look the blue sky ,enjoy

---
+++ random.cpp
@@ -0,0 +1,57 @@
+//random.cpp
+//demonstrates Reading a random access file swquentially
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include "ClientData.h" //ClientData class defination
+
+using namespace std;
+
+void outputLine( ostream&, const ClientData & );  //prototype
+
+int main()
+{
+idStream inCredit("credit.dat", ios::in | ios::binary );
+
+// exit program if ifstream cannot open file
+if (!inCredit)
+{
+cerr <<"File could not be opened." << endl;
+exit(EXIT_FALIURE );
+}//end if
+//output column heads
+cout<< left<< setw(10) <<"Account" <<setw(16)
+<<"Last Name"<<setw(11) <<"First Name" <<left<<setw(10)
+<<right << "Balance"<<endl;
+
+ClientData client;  //create record
+
+//read first record from file 
+inCredit.read(reinterpret_cast< char * > (&client ), 
+sizeof( ClientData));
+
+//read all records from file 
+while(inCredit && !inCredit.eof())
+{
+//display record
+if(client.getAccountNumber() != 0)
+outputLine(cout, client );
+
+//read next from file.
+inCredit.read( reinterpret_cast < char * > (&client ) ,
+sizeof(ClientData ));
+}//end while
+
+}//end main
+
+//display single record
+void outputLine( ostream &output, const ClientData &record )
+{
+output<<left<< setw(10) << record.getAccountNumber()
+<< setw( 16 ) <<record.getLastName()
+<< setw( 11 ) <<record.getFirstNAme()
+<< setw( 10 ) << setprecision(2) <<right << fixed
+<< showpoint << record.getBalance() << endl;
+}// end function outputLine.
